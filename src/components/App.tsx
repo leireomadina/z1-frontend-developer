570eb0966af9d3cd2b507c222687e0b68cf2c9f7
Leire Ordeñana <leireomadina@gmail.com>
2021-02-20 17:58:12 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import '../stylesheets/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hi Risingdana :)
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
